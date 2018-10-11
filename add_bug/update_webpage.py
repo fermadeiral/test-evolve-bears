4364674455cf9896d95913730be4156090c5c4ac
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-11 00:50:39 -0300

    Fix

--- add_bug/update_webpage.py
+++ add_bug/update_webpage.py
@@ -42,7 +42,5 @@ if bugs is not None:
     with open(os.path.join("docs", "data", "bears-bugs.json"), 'w') as fd:
         fd.write(json.dumps(bugs, indent=2))
 
-    commit_message = "Add " + branch
-
-    cmd = "git add -A; git commit -m %s; git push github;" % commit_message
+    cmd = "git add -A; git commit -m 'Add %s'; git push github;" % branch
     subprocess.call(cmd, shell=True)
