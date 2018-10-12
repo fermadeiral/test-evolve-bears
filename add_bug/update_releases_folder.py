f5f87813c536350907cf5e4d97ddbbfa47b82234
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-12 13:37:04 -0300

    Fix

--- add_bug/update_releases_folder.py
+++ add_bug/update_releases_folder.py
@@ -5,8 +5,7 @@ import json
 
 branch = sys.argv[1]
 
-txt_file_path = os.path.join("releases", "latest_branches.txt")
-json_file_path = os.path.join("releases", "branches_per_version.json")
+branches_per_version_file_path = os.path.join("releases", "branches_per_version.json")
 
 # check out master
 cmd = "git checkout -qf master;"
@@ -14,8 +13,8 @@ subprocess.call(cmd, shell=True)
 
 # update the json file containing all branches per version
 versions = None
-if os.path.exists(json_file_path):
-    with open(json_file_path,'r') as f:
+if os.path.exists(branches_per_version_file_path):
+    with open(branches_per_version_file_path,'r') as f:
         try:
             versions = json.load(f)
         except Exception as e:
@@ -24,7 +23,7 @@ if os.path.exists(json_file_path):
 if versions is not None:
     versions["latest"].append(branch)
 
-    with open(json_file_path,'w') as f:
+    with open(branches_per_version_file_path,'w') as f:
         f.write(json.dumps(versions, indent=2))
 
     cmd = "git add -A; git commit --amend --no-edit; git push -f github;"
