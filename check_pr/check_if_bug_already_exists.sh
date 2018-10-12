99537f2eb37e6809c4e1023573a85dc9d3665e63
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-12 15:46:38 -0300

    Fix

--- check_pr/check_if_bug_already_exists.sh
+++ check_pr/check_if_bug_already_exists.sh
@@ -10,14 +10,15 @@ branches_per_version_file_path="./releases/branches_per_version.json"
 
 git checkout -qf master
 
-jq -c '.[]' "$branches_per_version_file_path" | while read i; do
-    echo "$i"
-    if [ "$i" == "$BRANCH_NAME" ]; then
-        RESULT="> $BRANCH_NAME [FAILURE] (the bug already exists in Bears)"
-        echo -e "$RED""$RESULT""$NC"
-        exit 1
-    fi
-done
+repoName=$(echo "$branches_per_version_file_path" | grep "$BRANCH_NAME");
+
+echo "$repoName"
+
+#if [ "$i" == "$BRANCH_NAME" ]; then
+#    RESULT="> $BRANCH_NAME [FAILURE] (the bug already exists in Bears)"
+#    echo -e "$RED""$RESULT""$NC"
+#    exit 1
+#fi
 
 RESULT="> $BRANCH_NAME [OK]"
 echo -e "$GREEN""$RESULT""$NC"
