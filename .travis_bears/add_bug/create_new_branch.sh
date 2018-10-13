17c7ea7d272260b3c6c5e638ff4f7129bfe6e328
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-13 08:17:04 -0300

    Fix

--- .travis_bears/add_bug/create_new_branch.sh
+++ .travis_bears/add_bug/create_new_branch.sh
@@ -8,11 +8,11 @@ PATCHED_COMMIT_ID=$(git log --format=format:%H --grep="Human patch")
 END_COMMIT_ID=$(git log --format=format:%H --grep="End of the bug and patch reproduction process")
 
 git checkout --orphan "$NEW_BRANCH_NAME"; git reset .; git clean -fd;
-git cherry-pick "$bugCommitId";
-git cherry-pick "$testCommitId";
-git cherry-pick "$patchCommitId";
-git cherry-pick "$endCommitId";
+git cherry-pick "$BUGGY_COMMIT_ID";
+git cherry-pick "$TEST_COMMIT_ID";
+git cherry-pick "$PATCHED_COMMIT_ID";
+git cherry-pick "$END_COMMIT_ID";
 git push github "$NEW_BRANCH_NAME":"$NEW_BRANCH_NAME";
 
 git checkout pr-add-bug;
-python add_bug/update_bears_json_file.py "$NEW_BRANCH_NAME";
+python ./.travis_bears/add_bug/update_bears_json_file.py "$NEW_BRANCH_NAME";
--- .travis_bears/add_bug/update_data.sh
+++ .travis_bears/add_bug/update_data.sh
@@ -5,6 +5,7 @@ set -e
 git checkout pr-add-bug;
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*;
 git fetch;
-python add_bug/update_webpage.py "$NEW_BRANCH_NAME" "$TRAVIS_REPO_SLUG";
+python ./.travis_bears/add_bug/update_webpage.py "$NEW_BRANCH_NAME" "$TRAVIS_REPO_SLUG";
+
 git checkout pr-add-bug;
-python add_bug/update_releases_folder.py "$NEW_BRANCH_NAME";
+python ./.travis_bears/add_bug/update_releases_folder.py "$NEW_BRANCH_NAME";
