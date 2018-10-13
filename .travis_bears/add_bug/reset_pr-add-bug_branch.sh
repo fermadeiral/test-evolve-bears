fdae1b17693bffd979cee42588b46b6e1d2cfe6b
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-13 08:22:09 -0300

    Fix

--- .travis_bears/add_bug/reset_pr-add-bug_branch.sh
+++ .travis_bears/add_bug/reset_pr-add-bug_branch.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# git stash is used in the case that something wrong had happen in the previous scripts so it's still possible to reset
-# the branch pr-add-bug
+# git stash is used in the case that something wrong had happen in the previous scripts
+# so it's still possible to reset the branch pr-add-bug
 git stash;
 
-git checkout pr-add-bug;
-PARENTS_OF_THE_MERGED_COMMIT=$(git log --pretty=%P -n 1);
-LAST_COMMIT_BEFORE_MERGING=${PARENTS_OF_THE_MERGED_COMMIT%' '*};
-git reset --hard "$LAST_COMMIT_BEFORE_MERGING";
-git push -f github pr-add-bug;
+git checkout pr-add-bug
+PARENTS_OF_THE_MERGED_COMMIT=$(git log --pretty=%P -n 1)
+LAST_COMMIT_BEFORE_MERGING=${PARENTS_OF_THE_MERGED_COMMIT%' '*}
+git reset --hard "$LAST_COMMIT_BEFORE_MERGING"
+git push -f github pr-add-bug
