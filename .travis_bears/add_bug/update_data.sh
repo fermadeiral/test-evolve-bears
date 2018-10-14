aae40cb10e893a7a8b9b0d8963fe4e59c80ec0c0
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-14 07:49:14 -0300

    Fix

--- .travis_bears/add_bug/update_data.sh
+++ .travis_bears/add_bug/update_data.sh
@@ -4,7 +4,5 @@ set -e
 
 # the branch pr-add-bug is checked out
 
-git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git fetch
 python ./.travis_bears/add_bug/update_webpage.py "$NEW_BRANCH_NAME" "$TRAVIS_REPO_SLUG"
 python ./.travis_bears/add_bug/update_releases_folder.py "$NEW_BRANCH_NAME"
