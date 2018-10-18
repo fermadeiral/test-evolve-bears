94e3236775fdfa1adbaff986117b59bfdd88417d
Fer Madeiral <fer.madeiral@gmail.com>
2018-10-18 11:52:29 -0300

    Fix docker

--- .travis_bears/check_pr/check_with_maven/check_with_maven.sh
+++ .travis_bears/check_pr/check_with_maven/check_with_maven.sh
@@ -16,13 +16,6 @@ MAVEN_TEST_ARGS="-Denforcer.skip=true -Dcheckstyle.skip=true -Dcobertura.skip=tr
 
 cd pr
 
-BUGGY_BUILD_ID=$(jq -r '.["builds"]["buggyBuild"]["id"]' bears.json)
-POM_PATH=$(jq -r '.["reproductionBuggyBuild"]["projectRootPomPath"]' bears.json)
-POM_PATH=$(echo "$POM_PATH" | sed -e "s/.*$BUGGY_BUILD_ID\///g")
-POM_PATH=$(echo "$POM_PATH" | sed -e 's/pom.xml//g')
-
-cd "$POM_PATH"
-
 BUGGY_COMMIT_ID=""
 
 CASE=$(cat bears.json | sed 's/.*"type": "\(.*\)".*/\1/;t;d')
@@ -34,6 +27,15 @@ else
 fi
 
 PATCHED_COMMIT_ID=$(git log --format=format:%H --grep="Human patch")
+
+BUGGY_BUILD_ID=$(jq -r '.["builds"]["buggyBuild"]["id"]' bears.json)
+POM_PATH=$(jq -r '.["reproductionBuggyBuild"]["projectRootPomPath"]' bears.json)
+POM_PATH=$(echo "$POM_PATH" | sed -e "s/.*$BUGGY_BUILD_ID\///g")
+POM_PATH=$(echo "$POM_PATH" | sed -e 's/pom.xml//g')
+
+if [ ! -z "$POM_PATH" ]; then
+    cd "$POM_PATH"
+fi
 
 if [ "$IS_BUGGY_COMMIT" -eq 1 ]; then
 
